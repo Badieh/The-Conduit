574272dc01363691180061593218e3f62fa3e31f
Badieh Nader <badienader42@gmail.com>
2025-01-14 09:15:04 +0200

    fixing footer attribution

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -1,14 +1,30 @@
+import { paths } from "@/routes/AppRouter";
+import { Link } from "react-router";
+
 export default function Footer() {
   return (
     <footer>
       <div className="container">
-        <a href="/" className="logo-font">
+        <Link to={paths.home.getHref()} className="logo-font">
           conduit
-        </a>
+        </Link>
         <span className="attribution">
           An interactive learning project from{" "}
-          <a href="https://thinkster.io">Thinkster</a>. Code &amp; design
-          licensed under MIT.
+          <a href="https://thinkster.io" target="_blank" rel="noopener">
+            Thinkster
+          </a>
+          . Code &amp; design licensed under MIT.
+        </span>
+        <span className="attribution">
+          Implementation by{" "}
+          <a
+            href="https://www.linkedin.com/in/badiehnader/"
+            target="_blank"
+            rel="noopener"
+          >
+            Badieh nader
+          </a>
+          .
         </span>
       </div>
     </footer>
